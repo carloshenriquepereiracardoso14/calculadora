4ef8640ff43e40792baccc38755919333cba607c
carloshenriquepereiracardoso14 <carloscardosoesse@gmail.com>
2025-07-13 16:11:18 -0300

    web app

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "calculadora-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./img/icon-192.png",
+  "./img/icon-512.png"
+];
+
+// Instala o service worker e adiciona os arquivos ao cache
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Ativa e limpa caches antigos
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Responde com cache primeiro, depois rede
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) =>
+      response || fetch(event.request)
+    )
+  );
+});
